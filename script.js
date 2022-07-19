3bf221c01ae8781298a1f080330b45156a7eb594
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-07-18 18:32:42 -0700

    removing title from project details

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ var togglePages = function(button)
 		project = document.getElementById(projectID);
 		project.style.display = 'block';
 		project.querySelector('p').innerHTML = '';
+		project.querySelector('h1').style.display = 'block';
 	}
 };
 
@@ -45,8 +46,9 @@ var openProject = function(project)
 			if (projectID != project.id) document.getElementById(projectID).style.display = 'none';
 		}
 
-		// show project details
+		// show project details and hide title
 		project.querySelector('p').innerHTML = projects[project.id];
+		project.querySelector('h1').style.display = 'none';
 
 		// append to dir button
 		document.getElementById("dir").innerHTML += '<a onclick="togglePages(this)" href="#">' + project.id + '</a>/';
@@ -55,8 +57,8 @@ var openProject = function(project)
 
 var projects = 
 {
-	'magicleap': 'an exciting modeling app made for the magic leap headset built in C#',
-	'homelab': 'an ubuntu machine set up as a zfs storage server. Also hosts web servers and software tools like jupyter notebook using docker',
-	'ngrams': 'a natural language processing tool that counts the frequency of n-grams in a text file',
-	'anagrams': 'easy solution to your anagram!',
+	'magicleap': '<br><br>an exciting modeling app made for the magic leap headset built in C#',
+	'homelab': '<br><br><img width="700" src="homelab.svg"><br><p>an ubuntu machine set up as a zfs storage server. Also hosts web servers and software tools like jupyter notebook using docker</p>',
+	'ngrams': '<br><br>a natural language processing tool that counts the frequency of n-grams in a text file',
+	'anagrams': '<br><br>easy solution to your anagram!',
 };
