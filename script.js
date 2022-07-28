4f036d3ee1fac16959e754dcd9eb4156e3e9ad1f
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-07-27 20:55:34 -0700

    initial version of < and > buttons

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log('hello world');
 
+const back_button = document.getElementById('back');
+const forward_button = document.getElementById('forward');
 const directory_button = document.getElementById('path');
 const intro_page = document.getElementById('intro-page');
 const projects_page = document.getElementById('projects-page');
@@ -16,13 +18,38 @@ const projects =
 	//'add more': 'projects to the page as simple as adding another entry!',
 };
 
-var last_page = [] // stack that is appended to after clicking on new link
+var current_page = 'home'
+var last_page = [] // stack that is appended to after clicking on new link or forward button
 var next_page = [] // stack that is appended to with last page after clicking back
 
 var togglePages = function(button) 
 {
-	switch (button.id)
+	new_page = button.id;
+	console.log("New page -> " + new_page);
+	
+	// if using back or forward buttons
+	// set new_page accordingly
+	if (new_page == 'back')
 	{
+		next_page.push(current_page);
+		new_page = last_page.pop()
+	} else if (new_page == 'forward')
+	{
+		last_page.push(current_page);
+		new_page = next_page.pop();
+		last_page.push(current_page);
+	} else {
+		next_page = [];
+		last_page.push(current_page);
+	}
+
+	
+	
+	// update the current page
+	current_page = new_page;
+	
+	// toggle the page
+	switch (current_page) {
 		case 'home':
 			intro_page.style.display = 'block';
 			projects_page.style.display = 'none';
@@ -35,48 +62,58 @@ var togglePages = function(button)
 			projects_page.style.display = 'block';
 			directory_button.innerHTML = '<a id="home" onclick="togglePages(this)" href="#">~</a>/<a id="projects" onclick="togglePages(this)" href="#">projects</a>/';
 			website_title.innerHTML = '../projects/'
-			break;
-		case 'forward':
-			break;
-		case 'back':
+
+			// list projects, hide their details
+			for (let projectID of Object.keys(projects)) {
+				// if project hasn't been added to container yet, add it
+				if (!document.getElementById(projectID)) {
+					projects_container.innerHTML += '<div id="' + projectID + '" onclick="togglePages(this)"><h1><a href="#">' + projectID + '</a></h1><p></p></div'
+				}
+
+				// display name, hide details
+				let project = document.getElementById(projectID);
+				project.style.display = 'block';
+				project.querySelector('p').innerHTML = '';
+			}
+
 			break;
 		default:
-			// if project clicked on, do nothing
-			return;
+			
+			// opening project
+			project = document.getElementById(new_page);	
+
+			// open project
+			if (project.querySelector('p').innerHTML == '')
+			{
+				// hide other projects
+				for (let projectID of Object.keys(projects))
+				{
+					if (projectID != project.id) document.getElementById(projectID).style.display = 'none';
+				}
+				
+				// hide project title
+				project.querySelector('h1').style.display = 'none';
+				// show project details
+				project.querySelector('p').innerHTML = projects[project.id];
+
+				// append to dir button
+				directory_button.innerHTML += '<a onclick="togglePages(this)" href="#">' + project.id + '</a>/';
+				website_title.innerHTML = '../' + project.id + '/';
+			}
+			break;
 	}
-
-	// list projects, hide their details
-	for (let projectID of Object.keys(projects))
-	{
-		// if project hasn't been added to container yet, add it
-		if (!document.getElementById(projectID))
-		{
-			projects_container.innerHTML += '<div id="' + projectID + '" onclick="openProject(this)"><h1><a href="#">' + projectID + '</a></h1><p></p></div'
-		}
-
-		// display name, hide details
-		let project = document.getElementById(projectID);
-		project.style.display = 'block';
-		project.querySelector('p').innerHTML = '';
+	
+	console.log("last_page: " + last_page);
+	console.log("next_page: " + next_page);
+	console.log("");
+	if (next_page.length == 0){
+		forward_button.style.color = 'grey';
+	} else {
+		forward_button.style.color = '#20B2AA';
 	}
-};
-
-var openProject = function(project)
-{
-	// open project
-	if (project.querySelector('p').innerHTML == '')
-	{
-		// hide other projects
-		for (let projectID of Object.keys(projects))
-		{
-			if (projectID != project.id) document.getElementById(projectID).style.display = 'none';
-		}
-
-		// show project details
-		project.querySelector('p').innerHTML = projects[project.id];
-
-		// append to dir button
-		directory_button.innerHTML += '<a onclick="togglePages(this)" href="#">' + project.id + '</a>/';
-		website_title.innerHTML = '../' + project.id + '/';
+	if (last_page.length == 0){
+		back_button.style.color = 'grey';
+	} else {
+		back_button.style.color = '#20B2AA';
 	}
 };
