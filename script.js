79bdde5dadc2c86eef150410d03d1a38c6edaaa1
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-07-27 21:35:00 -0700

    removed duplicate function for > button

--- script.js
+++ script.js
@@ -19,16 +19,20 @@ const projects =
 };
 
 var current_page = 'home'
-var last_page = [] // stack that is appended to after clicking on new link or forward button
-var next_page = [] // stack that is appended to with last page after clicking back
+var last_page = []
+var next_page = []
 
 var togglePages = function(button) 
 {
+	// target page
 	new_page = button.id;
-	console.log("New page -> " + new_page);
-	
-	// if using back or forward buttons
-	// set new_page accordingly
+
+	// attempting to visit the current page
+	if (new_page == current_page) {
+		return;
+	}
+
+	// using back or forward buttons
 	if (new_page == 'back')
 	{
 		next_page.push(current_page);
@@ -37,53 +41,43 @@ var togglePages = function(button)
 	{
 		last_page.push(current_page);
 		new_page = next_page.pop();
-		last_page.push(current_page);
 	} else {
-		next_page = [];
 		last_page.push(current_page);
+		next_page = [];
 	}
 
-	
-	
-	// update the current page
-	current_page = new_page;
-
-	// toggle the page
-	switch (current_page) {
+	// toggle target page
+	switch (new_page) {
 		case 'home':
 			intro_page.style.display = 'block';
 			projects_page.style.display = 'none';
 			directory_button.innerHTML = '<a id="home" onclick="togglePages(this)" href="#">~</a>/';
 			website_title.innerHTML = '~/'
 			break;
-		case 'making':
 		case 'projects':
 			intro_page.style.display = 'none';
 			projects_page.style.display = 'block';
 			directory_button.innerHTML = '<a id="home" onclick="togglePages(this)" href="#">~</a>/<a id="projects" onclick="togglePages(this)" href="#">projects</a>/';
 			website_title.innerHTML = '../projects/'
 
-			// list projects, hide their details
+			// each project in dictionary
 			for (let projectID of Object.keys(projects)) {
-				// if project hasn't been added to container yet, add it
+				
+				// project hasn't been added to container yet
 				if (!document.getElementById(projectID)) {
 					projects_container.innerHTML += '<div id="' + projectID + '" onclick="togglePages(this)"><h1><a href="#">' + projectID + '</a></h1><p></p></div'
 				}
 
-				// display name, hide details
+				// display its title, hide details
 				let project = document.getElementById(projectID);
-				project.style.display = 'block';
-				project.style.pointerEvents = 'auto';
-				project.querySelector('p').innerHTML = '';
+				project.style.display = 'block'; // restore its visibility
+				project.style.pointerEvents = 'auto'; // restore its clickability
+				project.querySelector('p').innerHTML = ''; // hide its details
 			}
-
 			break;
 		default:
-			
-			// opening project
-			project = document.getElementById(new_page);	
-
-			// open project
+			// open a project
+			project = document.getElementById(new_page);
 			if (project.querySelector('p').innerHTML == '')
 			{
 				// hide other projects
@@ -99,26 +93,28 @@ var togglePages = function(button)
 				project.querySelector('p').innerHTML = projects[project.id];
 				// disable clickability
 				project.style.pointerEvents = 'none';
-
-				// append to dir button
-				directory_button.innerHTML += '<a onclick="togglePages(this)" href="#">' + project.id + '</a>/';
+				// append button to path
+				directory_button.innerHTML += '<a id="' + project.id + '" onclick="togglePages(this)" href="#">' + project.id + '</a>/';
+				// website title
 				website_title.innerHTML = '../' + project.id + '/';
 			}
 			break;
 	}
-	
-	console.log("last_page: " + last_page);
-	console.log("next_page: " + next_page);
-	console.log("");
 
-	if (next_page.length == 0){
+	// update the current page
+	current_page = new_page;
+	
+	// activate forward button?
+	if (next_page.length == 0) {
 		forward_button.style.color = 'grey';
 		forward_button.style.pointerEvents = 'none';
 	} else {
 		forward_button.style.color = '#20B2AA';
 		forward_button.style.pointerEvents = 'auto';
 	}
-	if (last_page.length == 0){
+
+	// activate back button?
+	if (last_page.length == 0) {
 		back_button.style.color = 'grey';
 		back_button.style.pointerEvents = 'none';
 	} else {
