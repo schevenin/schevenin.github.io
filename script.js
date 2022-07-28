a32585af952712d8ae61d355b289b0484045d6e0
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-07-28 01:46:45 -0700

    attempting a format for project details

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ const directory_button = document.getElementById('path');
 const intro_page = document.getElementById('intro-page');
 const projects_page = document.getElementById('projects-page');
 const projects_container = document.getElementById('projects-container');
-const website_title = document.querySelector('title');
 
 const projects = 
 {
-	'wip': 'this is a work in progress!',
-	'magicleap': 'a modeling app made for the magic leap headset built in C#',
-	'homelab': 'my all-in-one solution to NAS, and a remote development environment.',
-	'anagrams': 'easy solution to your difficult anagram!',
-	//'ngrams': 'a natural language processing tool that counts the frequency of n-grams in a text file',
-	//'add more': 'projects to the page as simple as adding another entry!',
+	'magicleap': 
+		'a modeling app made for the magic leap headset built in C#',
+	'homelab': 
+		'my all-in-one solution to NAS, and a remote development environment.',
+	'anagrams':
+		'easy solution to your difficult anagram!<br>\
+		<a href="https://github.com/schevenin/anagrams-solver/" target="_blank">github</a>',
+	//'ngrams': 
+	//	'a natural language processing tool that counts the frequency of n-grams in a text file',
+	//'add more': 
+	//	'projects to the page as simple as adding another entry!',
 };
 
 var current_page = 'home'
@@ -68,41 +72,39 @@ var togglePages = function(button)
 	updateNavButtons();
 };
 
-var openProject = function(project) {
-	// hide all other projects
-	for (let projectID of Object.keys(projects))
-	{
-		if (projectID != project.id) document.getElementById(projectID).style.display = 'none';
-			
-	}
-	
-	// hide project title
-	project.querySelector('h1').style.display = 'none';
-	// show project details
-	project.querySelector('p').innerHTML = projects[project.id];
-	// disable clickability
-	project.style.pointerEvents = 'none';
-	// append button to path
-	directory_button.innerHTML += '<a id="' + project.id + '" onclick="togglePages(this)" href="#">' + project.id + '</a>/';
-};
-
 var listProjects = function() {
 	// each project in dictionary
 	for (let projectID of Object.keys(projects)) {
 				
 		// project hasn't been added to container yet
 		if (!document.getElementById(projectID)) {
-			projects_container.innerHTML += '<div id="' + projectID + '" onclick="togglePages(this)"><h1><a href="#">' + projectID + '</a></h1><p></p></div'
+			projects_container.innerHTML += '<div id="' + projectID + '" onclick="togglePages(this)"><h1><a href="#">' + projectID + '</a></h1><div></div></div>'
 		}
 
-		// display its title, hide details
 		let project = document.getElementById(projectID);
-		project.style.display = 'block'; // restore its visibility
-		project.style.pointerEvents = 'auto'; // restore its clickability
-		project.querySelector('h1').style.display = 'block';
-		project.querySelector('p').innerHTML = ''; // hide its details
+		// restore project visibility
+		project.style.display = 'block'; 
+		// show project title
+		//project.firstChild.firstChild.innerHTML = projectID;
+		// hide project details
+		project.lastChild.innerHTML = '';
 	}
-}
+};
+
+var openProject = function(project) {
+	// hide all other projects
+	for (let projectID of Object.keys(projects)) {
+		if (projectID != project.id) document.getElementById(projectID).style.display = 'none';
+			
+	}
+	
+	// hide project title
+	//project.firstChild.firstChild.innerHTML = '';
+	// show project details
+	project.lastChild.innerHTML = projects[project.id];
+	// append button to path
+	directory_button.innerHTML += '<a id="' + project.id + '" onclick="togglePages(this)" href="#">' + project.id + '</a>/';
+};
 
 var updateNavButtons = function() {
 	// activate forward button?
