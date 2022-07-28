066a06d128bfb9dbd5849f1f70da3e97510ac094
Rogelio Schevenin Jr <rogelio@schevenin.com>
2022-07-27 23:56:36 -0700

    make code more readable with functions

--- script.js
+++ script.js
@@ -28,10 +28,8 @@ var togglePages = function(button)
 	new_page = button.id;
 
 	// attempting to visit the current page
-	if (new_page == current_page) {
-		return;
-	}
-
+	if (new_page == current_page) return;
+	
 	// using back or forward buttons
 	if (new_page == 'back')
 	{
@@ -46,65 +44,67 @@ var togglePages = function(button)
 		next_page = [];
 	}
 
+	// update the current page
+	current_page = new_page;
+
 	// toggle target page
-	switch (new_page) {
+	switch (current_page) {
 		case 'home':
 			intro_page.style.display = 'block';
 			projects_page.style.display = 'none';
 			directory_button.innerHTML = '<a id="home" onclick="togglePages(this)" href="#">~</a>/';
-			website_title.innerHTML = '~/'
 			break;
 		case 'projects':
 			intro_page.style.display = 'none';
 			projects_page.style.display = 'block';
 			directory_button.innerHTML = '<a id="home" onclick="togglePages(this)" href="#">~</a>/<a id="projects" onclick="togglePages(this)" href="#">projects</a>/';
-			website_title.innerHTML = '../projects/'
-
-			// each project in dictionary
-			for (let projectID of Object.keys(projects)) {
-				
-				// project hasn't been added to container yet
-				if (!document.getElementById(projectID)) {
-					projects_container.innerHTML += '<div id="' + projectID + '" onclick="togglePages(this)"><h1><a href="#">' + projectID + '</a></h1><p></p></div'
-				}
-
-				// display its title, hide details
-				let project = document.getElementById(projectID);
-				project.style.display = 'block'; // restore its visibility
-				project.style.pointerEvents = 'auto'; // restore its clickability
-				project.querySelector('h1').style.display = 'block';
-				project.querySelector('p').innerHTML = ''; // hide its details
-			}
+			listProjects();
 			break;
 		default:
-			// open a project
-			project = document.getElementById(new_page);
-			if (project.querySelector('p').innerHTML == '')
-			{
-				// hide other projects
-				for (let projectID of Object.keys(projects))
-				{
-					if (projectID != project.id) document.getElementById(projectID).style.display = 'none';
-						
-				}
-				
-				// hide project title
-				project.querySelector('h1').style.display = 'none';
-				// show project details
-				project.querySelector('p').innerHTML = projects[project.id];
-				// disable clickability
-				project.style.pointerEvents = 'none';
-				// append button to path
-				directory_button.innerHTML += '<a id="' + project.id + '" onclick="togglePages(this)" href="#">' + project.id + '</a>/';
-				// website title
-				website_title.innerHTML = '../' + project.id + '/';
-			}
+			openProject(document.getElementById(new_page));
 			break;
 	}
-
-	// update the current page
-	current_page = new_page;
 	
+	updateNavButtons();
+};
+
+var openProject = function(project) {
+	// hide all other projects
+	for (let projectID of Object.keys(projects))
+	{
+		if (projectID != project.id) document.getElementById(projectID).style.display = 'none';
+			
+	}
+	
+	// hide project title
+	project.querySelector('h1').style.display = 'none';
+	// show project details
+	project.querySelector('p').innerHTML = projects[project.id];
+	// disable clickability
+	project.style.pointerEvents = 'none';
+	// append button to path
+	directory_button.innerHTML += '<a id="' + project.id + '" onclick="togglePages(this)" href="#">' + project.id + '</a>/';
+};
+
+var listProjects = function() {
+	// each project in dictionary
+	for (let projectID of Object.keys(projects)) {
+				
+		// project hasn't been added to container yet
+		if (!document.getElementById(projectID)) {
+			projects_container.innerHTML += '<div id="' + projectID + '" onclick="togglePages(this)"><h1><a href="#">' + projectID + '</a></h1><p></p></div'
+		}
+
+		// display its title, hide details
+		let project = document.getElementById(projectID);
+		project.style.display = 'block'; // restore its visibility
+		project.style.pointerEvents = 'auto'; // restore its clickability
+		project.querySelector('h1').style.display = 'block';
+		project.querySelector('p').innerHTML = ''; // hide its details
+	}
+}
+
+var updateNavButtons = function() {
 	// activate forward button?
 	if (next_page.length == 0) {
 		forward_button.style.color = 'grey';
